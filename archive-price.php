6ce97323d7e93e28ddbdf8b95c41e3e427cd57ca
Goto daichi　 <goto.spiqa.design@gmail.com>
2025-11-06 20:58:26 +0900

    Update components, breadcrumbs, and add arrow icon

--- archive-price.php
+++ archive-price.php
@@ -2,11 +2,28 @@
 <?php wp_head(); ?>
 </head>
 
-<body class="">
+<body class="bl_commonLowPagebg">
     <?php get_header(); ?>
 
-    <main class="">
+    <main class="bl_commonLowPageWrapper">
+        <div class="bl_commonLowPageWrapper_ttlContainer">
+            <hgroup class="bl_commonLowPageWrapper_ttl">
+                <h1 class="el_commonLowPageWrapper_ttl_en">Price</h1>
+                <p class="el_commonLowPageWrapper_ttl_ja"><span>料金表</span></p>
+            </hgroup>
+        </div>
+        <div class="bl_commonLowPageWrapper_contentsOuter">
+            <div class="bl_commonLowPageWrapper_contents">
+                <div class="bl_commonLowPageWrapper_contents_inner">
+                    <div class="">
 
+                    </div>
+                    <div>
+                        <?php include(get_template_directory() . '/inc/breadcrumbs.php'); ?>
+                    </div>
+                </div>
+            </div>
+        </div>
     </main>
 
     <?php get_footer(); ?>
---
+++ inc/breadcrumbs.php
@@ -0,0 +1,13 @@
+<div class="bl_commonBreadcrumbsContainer">
+    <ul class="bl_commonBreadcrumbsList">
+        <li class="bl_commonBreadcrumbsItem">
+            <a href="<?php echo home_url(); ?>" class="bl_commonBreadcrumbsLink">トップ</a>
+        </li>
+        <li class="bl_commonBreadcrumbsItem bl_commonBreadcrumbsSeparator">
+            <img src="<?php echo get_template_directory_uri(); ?>/assets/img/common/arrow-icon.svg" alt="">
+        </li>
+        <li class="bl_commonBreadcrumbsItem">
+            <p class="bl_commonBreadcrumbsText"></p>
+        </li>
+    </ul>
+</div>
