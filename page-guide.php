34de06bf41a1163956b7450d65f5c932b0c95425
Goto daichi　 <goto.spiqa.design@gmail.com>
2025-12-02 16:25:16 +0900

    初めての方へページ

---
+++ page-guide.php
@@ -0,0 +1,37 @@
+<?php
+
+/**
+ * Template Name: Guide
+ */
+?>
+
+<?php get_header('meta'); ?>
+<?php wp_head(); ?>
+</head>
+
+<body class="bl_commonLowPagebg">
+    <?php get_header(); ?>
+
+    <main class="bl_commonLowPageWrapper">
+        <div class="bl_commonLowPageWrapper_ttlContainer">
+            <hgroup class="bl_commonLowPageWrapper_ttl">
+                <h1 class="el_commonLowPageWrapper_ttl_en">Guide</h1>
+                <p class="el_commonLowPageWrapper_ttl_ja"><span>初めての方へ</span></p>
+            </hgroup>
+        </div>
+        <div class="bl_commonLowPageWrapper_contentsOuter">
+            <div class="bl_commonLowPageWrapper_contents">
+                <div class="bl_commonLowPageWrapper_contents_inner">
+
+                    <div>
+                        <?php include(get_template_directory() . '/inc/breadcrumbs.php'); ?>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </main>
+
+    <?php get_footer(); ?>
+</body>
+
+</html>
